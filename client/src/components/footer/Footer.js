0882a4a8cb7823644fb8c06dc2dd458a14969027
bernardfernando <jagathf@gmail.com>
2023-08-09 01:56:24 +0100

    amends

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -3,6 +3,15 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
+      <div className="footer1">
+        <div className="address">
+          <p>Tec Ed School</p>
+          <p>Pottersgate</p>
+          <p>Norwich NR1</p>
+        </div>
+        <div className="tel">01603 888 7777</div>
+      </div>
+
       <p>301 final project</p>
     </footer>
   );
