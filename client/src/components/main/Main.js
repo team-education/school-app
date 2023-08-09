a2dbbd08d28a128493f465e185c4131555dbb912
bernardfernando <jagathf@gmail.com>
2023-08-09 10:24:25 +0100

    added routing

---
+++ client/src/components/main/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Main.css";
+import CustomCarousel from "../carousel/Carousel";
+
+export default function Main() {
+  return (
+    <div className="Main">
+      <CustomCarousel />
+    </div>
+  );
+}
