902b53aaac94bcfa388e746536990da23bdfe9ca
CHIDI ONONYE <fononye2020@gmail.com>
2023-08-11 10:29:10 +0100

    put the student list in a table. Also, styled with borthers and others

--- client/src/components/StudentRegister.js
+++ client/src/components/StudentRegister.js
@@ -33,26 +33,63 @@ export default function StudentRegister() {
     setStudents((prevStudents) => [...prevStudents, newStudent]);
   }
 
-  return (
-    <div>
-      <h2>Student Register</h2>
-      (<div><AddStudent /> <handleDelete /> </div>) {/* Assuming this component allows adding new students */}
+//   return (
+//     <div>
+//       <h2>Student Register</h2>
+//       (<div><AddStudent /> <handleDelete /> </div>) {/* Assuming this component allows adding new students */}
+//       {students.length > 0 ? (
+//         <ul>
+//           {students.map((student, index) => (
+//             <li key={index}>
+//               <strong>Name:</strong> {student.name}
+//               <br />
+//               <strong>Age:</strong> {student.age}
+//               <br />
+//               <strong>Grade:</strong> {student.grade}
+//               <p onClick={() => handleDelete(student._id)}>X</p>
+//             </li>
+//           ))}
+//         </ul>
+//       ) : (
+//         <p>No students available.</p>
+//       )}
+//     </div>
+//   );
+// }
+
+
+return (
+  <div className="student-register">
+    <h2>Student Register</h2>
+    <AddStudent />
+    <div className="table-container">
       {students.length > 0 ? (
-        <ul>
-          {students.map((student, index) => (
-            <li key={index}>
-              <strong>Name:</strong> {student.name}
-              <br />
-              <strong>Age:</strong> {student.age}
-              <br />
-              <strong>Grade:</strong> {student.grade}
-              <p onClick={() => handleDelete(student._id)}>X</p>
-            </li>
-          ))}
-        </ul>
+        <table className="student-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Grade</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {students.map((student, index) => (
+              <tr key={index}>
+                <td>{student.name}</td>
+                <td>{student.age}</td>
+                <td>{student.grade}</td>
+                <td>
+                  <button onClick={() => handleDelete(student._id)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       ) : (
         <p>No students available.</p>
       )}
     </div>
-  );
-}
+  </div>
+);
+      }
